9391028d36bfaf4b4d4790439f56831eddc0b12c
Kamil Sharipov <kamil5812@gmail.com>
2026-02-28 19:03:20 +0300

    refactoring: simple fixes

--- include/system.hpp
+++ include/system.hpp
@@ -11,8 +11,8 @@ constexpr size_t PUCCH_STD_BIT_SIZE = 20;
 
 using Complex = std::complex<double>;
 
+} // namespace qpsk
+
 int run_coding_mode(const nlohmann::json& input, nlohmann::json& output);
 int run_decoding_mode(const nlohmann::json& input, nlohmann::json& output);
 int run_simulation_mode(const nlohmann::json& input, nlohmann::json& output);
-
-} // namespace qpsk
